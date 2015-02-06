f24c9f153a66dcb8eb2169b77fcad3b0cce775f7
Breena Theseira <breenatheseira@yahoo.com>
2015-02-06 09:43:12 +0000

    Added: Avatar uploader.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,7 @@
 class ImagesController < ApplicationController
+    require 'carrierwave/orm/activerecord'
     def index
+        uploader = AvatarUploader.new
+        #@test = uploader.retrieve_from_store!('default.jpg')
     end
 end
