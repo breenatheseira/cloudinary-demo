58ecfa2bbfdad6d9a1e5c4ef5e926f6b587b5837
Breena Theseira <breenatheseira@yahoo.com>
2015-02-06 10:38:59 +0000

    Added: Image resource for image routing in order to save route, upload a file and display file.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,28 @@
 class ImagesController < ApplicationController
     require 'carrierwave/orm/activerecord'
     def index
-        uploader = AvatarUploader.new
+        @image = Image.new
+        #uploader = AvatarUploader.new
         #@test = uploader.retrieve_from_store!('default.jpg')
+    end
+    
+    def create
+        # uploader = AvatarUploader.new
+        @image = Image.new(image_params)
+        # uploader.store!(@image.avatar)
+
+        if @image.save
+            redirect_to image_path(@image)
+        end
+    end
+    
+    def show
+        @image = Image.find(params[:id])
+        # uploader = AvatarUploader.new
+        # @my_file = uploader.retrieve_from_store!(@image.avatar)
+    end
+    
+    def image_params
+        params.require(:image).permit(:name,:avatar)
     end
 end
