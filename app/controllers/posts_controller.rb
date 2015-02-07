015c4e9e58bdb61dec6e99b6f3ffab1fe525d5ab
Breena Theseira <breenatheseira@yahoo.com>
2015-02-07 15:47:21 +0000

    Added: Views for posting pictures.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path(@post)
+        end
+    end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+    
+    def post_params
+        params.require(:post).permit(:picture,:picture_cache)
+    end
 end
